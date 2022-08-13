e22a3959c007bc62175e7b781e27505772619831
bnazli <bekirnazligul2@gmail.com>
2022-08-13 12:18:26 +0300

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+var r1 = 0;
+var r2 = 0;
+var r3 = 0;
+var r4 = 0;
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
